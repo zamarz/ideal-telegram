627bc3edea401f230cbfa5736bfed6eb91dc5b83
zamarz <zachmarzouk@gmail.com>
2023-10-31 11:48:35 +0000

    Update Loading spinner

--- components/Loading.jsx
+++ components/Loading.jsx
@@ -2,10 +2,16 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <button type="button" className="bg-indigo-500" disabled>
-      <svg className="  animate-spin h-5 w-5 mr-3" viewBox="0 0 24 24"></svg>
-      Processing...
-    </button>
+    <div className="flex justify-center">
+      <div
+        class="animate-spin inline-block w-6 h-6 border-[3px] border-current border-t-transparent text-pink2 rounded-full"
+        role="status"
+        aria-label="loading"
+      >
+        <span className="sr-only">Loading...</span>
+      </div>
+      <h2 className="text-center  text-2xl  tracking-wide px-2">Loading...</h2>
+    </div>
   );
 };
 
